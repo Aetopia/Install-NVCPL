eebb47798cede3573c3a35b84bc437c546b20b50
Aetopia <kirbymario22@gmail.com>
2023-02-21 17:48:35 +0530

    Update Install-NVCPL.ps1

--- Install-NVCPL.ps1
+++ Install-NVCPL.ps1
@@ -1,11 +1,11 @@
-if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
     choice.exe /C 12 /N /M "Install NVIDIA Control Panel as:`n1. Win32 App`n2. UWP App`n>"
 
     $NVCPL = "$ENV:TEMP\NVCPL.zip"
     $InstallationDirectory = "$ENV:PROGRAMDATA\NVIDIA Corporation\NVCPL"
     $ShortcutFile = "$ENV:PROGRAMDATA\Microsoft\Windows\Start Menu\Programs\NVIDIA Control Panel.lnk"
-    if ($UWP) { $NVCPL = "$NVCPL.appx" }
+    if ($LASTEXITCODE -eq 2) { $NVCPL = "$NVCPL.appx" }
     if ($null -eq (Get-CimInstance Win32_VideoController | Where-Object { $_.Name -like "NVIDIA*" })) { Write-Error "No NVIDIA GPU found." -ErrorAction Stop }
 
     # Disable Telemetry.
@@ -25,29 +25,30 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
         ForEach-Object { ((($_ -split "<td>", 2, "SimpleMatch")[1] -Split "rel=", 2, "SimpleMatch")[0] -Split "<a href=", 2, "SimpleMatch")[1].Trim().Trim('"') })[-1]
     Invoke-RestMethod "$Link" -OutFile "$NVCPL"
 
-    if ($UWP) {
+    if ($LASTEXITCODE -eq 2) {
         Write-Output "Installing the NVIDIA Control Panel as an UWP app..."
         Add-AppxPackage "$NVCPL" -ForceApplicationShutdown -ForceUpdateFromAnyVersion
-        Write-Output "NVIDIA Control Panel Installed!"
-        return
     }
+    else {
 
-    Write-Output "Installing the NVIDIA Control Panel as a Win32 app..."
-    # Disable the NVIDIA Root Container Service. The NVIDIA Control Panel Launcher runs the service when the NVIDIA Control Panel is launched.
-    Stop-Process -Name "NVDisplay.Container" -Force -ErrorAction SilentlyContinue
-    Set-Service "NVDisplay.ContainerLocalSystem" -StartupType Disabled -ErrorAction SilentlyContinue
-    Stop-Service "NVDisplay.ContainerLocalSystem" -Force -ErrorAction SilentlyContinue
-    foreach ($i in ($InstallationDirectory, $ShortcutFile)) { Remove-Item "$i" -Recurse -Force -ErrorAction SilentlyContinue }
-    Expand-Archive "$NVCPL" "$InstallationDirectory" -Force
+        Write-Output "Installing the NVIDIA Control Panel as a Win32 app..."
+        # Disable the NVIDIA Root Container Service. The NVIDIA Control Panel Launcher runs the service when the NVIDIA Control Panel is launched.
+        Stop-Process -Name "NVDisplay.Container" -Force -ErrorAction SilentlyContinue
+        Set-Service "NVDisplay.ContainerLocalSystem" -StartupType Disabled -ErrorAction SilentlyContinue
+        Stop-Service "NVDisplay.ContainerLocalSystem" -Force -ErrorAction SilentlyContinue
+        foreach ($i in ($InstallationDirectory, $ShortcutFile)) { Remove-Item "$i" -Recurse -Force -ErrorAction SilentlyContinue }
+        Expand-Archive "$NVCPL" "$InstallationDirectory" -Force
 
-    # This launcher is needed inorder to suppress the annoying pop-up that the UWP Control Panel isn't installed.
-    Invoke-RestMethod "$((Invoke-RestMethod "https://api.github.com/repos/Aetopia/Install-NVCPL/releases/latest").assets.browser_download_url)" -OutFile "$InstallationDirectory\nvcpl.exe"
-    $WSShell = New-Object -ComObject "WScript.Shell"
-    $Shortcut = $WSShell.CreateShortcut("$ShortcutFile")
-    $Shortcut.TargetPath = "$InstallationDirectory\nvcpl.exe"
-    $Shortcut.IconLocation = "$InstallationDirectory\nvcplui.exe, 0"
-    $Shortcut.Save()
+        # This launcher is needed inorder to suppress the annoying pop-up that the UWP Control Panel isn't installed.
+        Invoke-RestMethod "$((Invoke-RestMethod "https://api.github.com/repos/Aetopia/Install-NVCPL/releases/latest").assets.browser_download_url)" -OutFile "$InstallationDirectory\nvcpl.exe"
+        $WSShell = New-Object -ComObject "WScript.Shell"
+        $Shortcut = $WSShell.CreateShortcut("$ShortcutFile")
+        $Shortcut.TargetPath = "$InstallationDirectory\nvcpl.exe"
+        $Shortcut.IconLocation = "$InstallationDirectory\nvcplui.exe, 0"
+        $Shortcut.Save()
+    }
     Write-Output "NVIDIA Control Panel Installed!"
-} else {
+}
+else {
     Write-Error "Run this script as an Administrator." -ErrorAction Stop
 }
