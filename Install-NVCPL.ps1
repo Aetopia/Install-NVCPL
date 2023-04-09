38ffeebbc22b269c39014661ea6617d6aa42a57d
Aetopia <41850963+Aetopia@users.noreply.github.com>
2023-04-09 13:05:01 +0530

    Configure NVIDIA Control Panel.

--- Install-NVCPL.ps1
+++ Install-NVCPL.ps1
@@ -1,16 +1,33 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
-    choice.exe /C 12 /N /M "Install NVIDIA Control Panel as:`n1. Win32 App`n2. UWP App`n>"
-
+    choice.exe /C 123 /N /M "NVIDIA Control Panel Installer:`n1. Install as Win32 App`n2. Install as UWP App`n3. Uninstall`n>"
     $NVCPL = "$ENV:TEMP\NVCPL.zip"
+    $NVIDIACorporation = "HKCU:\Software\NVIDIA Corporation"
+    $NVCPLConfigurationKeys = @("NVControlPanel2\Client", "Global\NvCplApi\Policies", "NvTray" )
     $InstallationDirectory = "$ENV:PROGRAMFILES\NVIDIA Corporation\Control Panel Client"
     $ShortcutFile = "$ENV:PROGRAMDATA\Microsoft\Windows\Start Menu\Programs\NVIDIA Control Panel.lnk"
     if ($LASTEXITCODE -eq 2) { $NVCPL = "$NVCPL.appx" }
     if ($null -eq (Get-CimInstance Win32_VideoController | Where-Object { $_.Name -like "NVIDIA*" })) { Write-Error "No NVIDIA GPU found." -ErrorAction Stop }
 
-    # Disable Telemetry.
+    if ($LASTEXITCODE -eq 3) {
+        Write-Output "Uninstalling the NVIDIA Control Panel..."
+        Get-AppxPackage "NVIDIACorp.NVIDIAControlPanel" | Remove-AppxPackage
+        Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$InstallationDirectory\nvcplui.exe" -ErrorAction SilentlyContinue
+        @($InstallationDirectory, $ShortcutFile) | ForEach-Object { Remove-Item "$_" -Recurse -Force -ErrorAction SilentlyContinue }
+        Write-Output "NVIDIA Control Panel Uninstalled!"
+        return
+    };
+    
+    # Setup NVIDIA Control Panel.
+    Remove-Item "$NVIDIACorporation" -Force -Recurse -ErrorAction SilentlyContinue
+    New-Item "$NVIDIACorporation" | Out-Null
+    $NVCPLConfigurationKeys | 
+    ForEach-Object { New-Item "$NVIDIACorporation\$_" -Force -ErrorAction SilentlyContinue | Out-Null }
+    New-ItemProperty  "$NVIDIACorporation\$($NVCPLConfigurationKeys[0])" -Name "ShowSedoanEula" -Value 1 -PropertyType DWORD | Out-Null
     New-ItemProperty -Path "HKLM:\SOFTWARE\NVIDIA Corporation\NvControlPanel2\Client" -Name "OptInOrOutPreference" -Value 0 -PropertyType DWORD -Force -ErrorAction SilentlyContinue | Out-Null
     New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\nvlddmkm\Global\Startup" -Name "SendTelemetryData" -Value 0 -PropertyType DWORD -Force -ErrorAction SilentlyContinue | Out-Null
+
+
     # Using rg-adguard to fetch the latest version of the NVIDIA Control Panel.
     $Body = @{
         type = 'url'
@@ -32,16 +49,17 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
     else {
 
         Write-Output "Installing the NVIDIA Control Panel as a Win32 app..."
-
-        # Run the NVIDIA Control Panel as an Administrator.
-        New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -ErrorAction SilentlyContinue | Out-Null
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$InstallationDirectory\nvcplui.exe" -Value "~ RUNASADMIN" -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
         
-        # Disable the NVIDIA Root Container Service. The service runs when the NVIDIA Control Panel is launched.
+        # Configure the NVIDIA Control Panel.
+        New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name "$InstallationDirectory\nvcplui.exe" -Value "~ RUNASADMIN" -PropertyType String -Force -ErrorAction SilentlyContinue | Out-Null
+        New-ItemProperty "$NVIDIACorporation\$($NVCPLConfigurationKeys[1])" -Name "ContextUIPolicy" -Value 0 -PropertyType DWORD | Out-Null
+        New-ItemProperty  "$NVIDIACorporation\$($NVCPLConfigurationKeys[2])" -Name "StartOnLogin" -Value 0 -PropertyType DWORD | Out-Null
+
         Stop-Process -Name "NVDisplay.Container" -Force -ErrorAction SilentlyContinue
         Set-Service "NVDisplay.ContainerLocalSystem" -StartupType Disabled -ErrorAction SilentlyContinue
         Stop-Service "NVDisplay.ContainerLocalSystem" -Force -ErrorAction SilentlyContinue
-        foreach ($File in ($InstallationDirectory, $ShortcutFile)) { Remove-Item "$File" -Recurse -Force -ErrorAction SilentlyContinue }
+        @($InstallationDirectory, $ShortcutFile) | ForEach-Object { Remove-Item "$_" -Recurse -Force -ErrorAction SilentlyContinue }
         Expand-Archive "$NVCPL" "$InstallationDirectory" -Force
 
         # This DLL is needed inorder to suppress the annoying pop-up that says the UWP Control Panel isn't installed.
