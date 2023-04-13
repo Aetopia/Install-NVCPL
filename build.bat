d7ad3c29345d3b50a3ad047925bb4354923245b8
Aetopia <41850963+Aetopia@users.noreply.github.com>
2023-04-13 23:41:12 +0530

    Auto terminate "NVDisplay.Container.exe"

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 cd "%~dp0"
-gcc -shared -s -Os nvcpluir.c -o nvcpluir.dll
+gcc -shared -s -Os nvcpluir.c -lwtsapi32 -o nvcpluir.dll
 upx --best --ultra-brute nvcpl.exe>nul 2>&1
--- nvcpluir.c
+++ nvcpluir.c
@@ -1,14 +1,17 @@
 #include <windows.h>
 #include <shlobj.h>
+#include <wtsapi32.h>
 
 BOOL DllMain(
     HINSTANCE hinstDLL,
     DWORD fdwReason,
     LPVOID lpvReserved)
 {
-    static DWORD dwBytesNeeded;
+    static WTS_PROCESS_INFOW *pWPI;
+    static DWORD dwBytesNeeded, dwCount;
     static SERVICE_STATUS_PROCESS ssp;
     static SC_HANDLE hSCManager, hService;
+    static HANDLE hProcess;
 
     if (IsUserAnAdmin())
         if (fdwReason == DLL_PROCESS_ATTACH)
@@ -22,17 +25,24 @@ BOOL DllMain(
             StartServiceW(hService, 0, NULL);
             ChangeServiceConfigW(hService, SERVICE_NO_CHANGE, SERVICE_DISABLED, SERVICE_NO_CHANGE, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
             do
-            {
                 QueryServiceStatusEx(hService, SC_STATUS_PROCESS_INFO, (LPBYTE)&ssp, sizeof(SERVICE_STATUS_PROCESS), &dwBytesNeeded);
-            } while (ssp.dwCurrentState != SERVICE_RUNNING);
+            while (ssp.dwCurrentState != SERVICE_RUNNING);
         }
         else if (fdwReason == DLL_PROCESS_DETACH)
         {
             ControlService(hService, SERVICE_CONTROL_STOP, (LPSERVICE_STATUS)&ssp);
-            do
+            WTSEnumerateProcessesW(WTS_CURRENT_SERVER, 0, 1, &pWPI, &dwCount);
+            for (DWORD i = 0; i < dwCount; i++)
             {
+                if (wcscmp(pWPI[i].pProcessName, L"NVDisplay.Container.exe"))
+                    continue;
+                hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pWPI[i].ProcessId);
+                TerminateProcess(hProcess, 0);
+                CloseHandle(hProcess);
+            };
+            do
                 QueryServiceStatusEx(hService, SC_STATUS_PROCESS_INFO, (LPBYTE)&ssp, sizeof(SERVICE_STATUS_PROCESS), &dwBytesNeeded);
-            } while (ssp.dwCurrentState != SERVICE_STOPPED);
+            while (ssp.dwCurrentState != SERVICE_STOPPED);
             CloseServiceHandle(hSCManager);
             CloseServiceHandle(hService);
         };
